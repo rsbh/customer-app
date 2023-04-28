4d567191b38fd2afc2eb042672024a66bea395bf
Rishabh Mishra <me@rsbh.dev>
2023-04-29 01:03:13 +0530

    add customer db model

--- api/customers/customer.go
+++ api/customers/customer.go
@@ -6,9 +6,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func listCustomersHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, "list customers")
+}
+
+func getCustomerHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, "get customers")
+}
+
+func createCustomerHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, "create customers")
+}
+
+func updateCustomerHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, "update customers")
+}
+
 func BindRoutes(rg *gin.RouterGroup, baseURL string) {
 	router := rg.Group(baseURL)
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "customers")
-	})
+	router.GET("/", listCustomersHandler)
+	router.POST("/", createCustomerHandler)
+	router.GET("/:id", getCustomerHandler)
+	router.PUT("/:id", updateCustomerHandler)
 }
---
+++ db/schema/customer.go
@@ -0,0 +1,12 @@
+package schema
+
+import "time"
+
+type Customer struct {
+	ID        string    `db:"id"`
+	FirstName string    `db:"first_name"`
+	LastName  string    `db:"last_name"`
+	Email     string    `db:"email"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
