8c8b1a04636bae4c26446ec19066870f3732534d
Rishabh Mishra <me@rsbh.dev>
2023-04-28 20:02:42 +0530

    add gin server

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getServer() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return r
+}
+
+func main() {
+	server := getServer()
+	server.Run()
+}
